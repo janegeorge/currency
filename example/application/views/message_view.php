ef157c2d28cf5ea5a5f1558c0132e9a96087a59c
janegeorge <jgeorge@tcd.ie>
2016-09-19 14:55:22 +0100

    Update message_view.php

--- example/application/views/message_view.php
+++ example/application/views/message_view.php
@@ -6,8 +6,9 @@
             <!--/ page title -->
    <main class="page-content">
                 <div class="grid-row">
+                
      <form action="" id="ajaxform"  method="post" class="bossform">
-         
+                               
             <?php $attributes = 'class="form-control" id="currency_from"';
             echo form_dropdown('currency_from', $getcurrency, 
             set_value('currency_from','id="currency_from"'), $attributes); ?>
@@ -26,16 +27,23 @@
             set_value('countryname','id="countryname"'), $attributes); ?>
         </div> 
         <br>
-        
+         <input type="hidden" value="<?php echo $this->session->userdata('userid');?>" name="userid"/>
+         
        <input type="submit" id="submit" class="button" value="submit" /> 
          <div id="status"></div>        
       </form> 
+         <br>
+           <form action="<?php echo base_url()?>listmessage" method="post" class="bossform">
+            <input type="hidden" value="<?php echo $this->session->userdata('userid');?>" name="userid"/>
+               <input type="submit" id="submit" class="button" value="View Message" />
+           </form>
                      </div>  
    </main> 
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
    <script type="text/javascript">  
 $(document).ready(function() {
   $("#ajaxform").submit(function(e){
+    // e.preventDefault();
     var contactdata  =   $(this).serializeArray();
     var submiturl    =  $(this).attr('action');
     var submitbtn    =  $('#submit');
@@ -48,15 +56,12 @@ $(document).ready(function() {
       data : contactdata,
       async:true,
       success: function(response){ 
-                //alert(data);
                 console.log(response.status);
                 if(response.status=true){
                 $("select").each(function() { this.selectedIndex = 0 });
                 document.getElementById('amountsell').value='';
                  document.getElementById('amountbuy').value='';
                   $('#status').html(response.message);
-                
-                
                 submitbtn.val('SUBMIT');
       }
       else{
@@ -66,10 +71,14 @@ $(document).ready(function() {
        
         } 
     });
+   // e.preventDefault(); 
   }); 
 });
 </script>
 
+
+ 
+  
 
  
   

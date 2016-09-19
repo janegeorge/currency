cebd79e165ebc3948aa4dabcb2262c4dddea640a
janegeorge <jgeorge@tcd.ie>
2016-09-19 10:50:06 +0100

    Update login_model.php

--- example/application/models/login_model.php
+++ example/application/models/login_model.php
@@ -1,16 +1,10 @@
   <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Login_model extends CI_Model
 { 
- function insertdata($user,$pwd)
-    {
-        $data=array(
-         'uname'=>$user,
-         'pwd'=>$pwd
-        );
-        $this->db->insert('new',$data);
-        
-        
-    } 
+    /*
+     Function name:getdata
+    Description:checking the username password is correct or not
+    */
     function getdata($user,$pwd)
     {
    $this -> db -> select('user_name,password,userid');
@@ -28,49 +22,7 @@ class Login_model extends CI_Model
    else
    {
      return false;
-   }
- 
-        
+   }      
     }
-    public function insertbusiness($add)
-    {
-        $data=array(
-         'name'=>$add
-        );
-        $this->db->insert('business',$data);
-    }
-    public function getbusinessdata()
-    {
-        $this->db->select('*');
-        $this->db->from('business');
-        $result=$this->db->get();
-        return $result;
-        
-    }
-    public function editdata($id)
-    {
-        $this->db->select('name,id');
-        $this->db->from('business');
-        $this->db->where('id',$id);
-        $result=$this->db->get();
-        echo $this->db->last_query();
-        return $result;
-        
-    }
-    public function getupdate($id,$name)
-    {
-        $data=array(
-         'name'=>$name
-        );
-        $this->db->where('id',$id);
-        $this->db->update('business',$data);
-        
-    }
-    public function deletedata($id)
-    {
-        $this->db->where('id',$id);
-        $this->db->delete('business');
-        
-       
-    }
+    
 }  

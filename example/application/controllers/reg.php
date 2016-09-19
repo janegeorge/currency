a42ce415788d222a99f61a950159af76a1fb1d5d
janegeorge <jgeorge@tcd.ie>
2016-09-19 10:40:27 +0100

    Update reg.php

--- example/application/controllers/reg.php
+++ example/application/controllers/reg.php
@@ -1,22 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Reg extends CI_Controller {
-
-    /**
-     * Index Page for this controller.
-     *
-     * Maps to the following URL
-     *         http://example.com/index.php/welcome
-     *    - or -  
-     *         http://example.com/index.php/welcome/index
-     *    - or -
-     * Since this controller is set as the default controller in 
-     * config/routes.php, it's displayed at http://example.com/
-     *
-     * So any other public methods not prefixed with an underscore will
-     * map to /index.php/welcome/<method_name>
-     * @see http://codeigniter.com/user_guide/general/urls.html
-     */
     public function index()
     {
         {
@@ -25,7 +9,11 @@ class Reg extends CI_Controller {
             $this->load->view("footer1.php");
         }
     }
-    
+     /*
+     Function Name:submit
+     Description:validating all the fields and and after that inserting the 
+     data to the database
+     */
     public function submit()
     {
         $this->form_validation->set_error_delimiters('<div style="color:#B94A48">', '</div>');                                                                                                                                                                                                   
